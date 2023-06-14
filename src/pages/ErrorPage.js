95677d1f6e79fc89962b94cf4ca38eec832c8546
Nandha Kumar <nandhakumar454kpm@gmail.com>
2023-06-14 18:37:35 +0530

    initial commit

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+const ErrorPage = () => {
+  return <Wrapper className='page-100'>
+     <section>
+      <h2>404 Error</h2>
+      <h2>Sorry,cannot load the page. Page not found</h2>
+      <Link to='/' className='btn'>
+          Back Home
+      </Link>
+     </section>
+  </Wrapper>
+}
+
+const Wrapper = styled.main`
+  background: var(--clr-primary-10);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  h1 {
+    font-size: 10rem;
+  }
+  h3 {
+    text-transform: none;
+    margin-bottom: 2rem;
+  }
+`
+
+export default ErrorPage
